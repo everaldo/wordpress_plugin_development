38dadd7ef0435ef4406968955fb6d282d50cf9b3
Everaldo Gomes <everaldo.gomes@gmail.com>
2012-11-27 10:27:11 -0200

    Update settings in wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -14,6 +14,15 @@
  * @package WordPress
  */
 
+//** Everaldo Gomes 27/11/2012
+define('WP_SITEURL', 'http://' . $_SERVER['SERVER_NAME'] . '/wordpress');
+define('WP_HOME',    'http://' . $_SERVER['SERVER_NAME']);
+
+
+//** Everaldo Gomes 27/11/2012
+define('WP_CONTENT_DIR', $_SERVER['DOCUMENT_ROOT'] . '/wp-content');
+define('WP_CONTENT_URL', 'http://' . $_SERVER['SERVER_NAME'] . '/wp-content');
+
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define('DB_NAME', 'database_name_here');
